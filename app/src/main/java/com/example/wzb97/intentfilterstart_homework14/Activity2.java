e3f1547d540c45fdf50dc63bb5eef9e0a7d7842b
wzb9706 <wzb9706@sina.com>
2016-09-05 01:15:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wzb97/intentfilterstart_homework14/Activity2.java
@@ -0,0 +1,16 @@
+package com.example.wzb97.intentfilterstart_homework14;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by wzb97 on 2016/9/5.
+ */
+public class Activity2 extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2);
+
+    }
+}
---
+++ app/src/test/java/com/example/wzb97/intentfilterstart_homework14/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.wzb97.intentfilterstart_homework14;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
